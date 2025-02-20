413e4b0a6e64a6106458bf242196eba210ec2c68
Subhradeep Kundu <kundusubhradeep2003@gmail.com>
2025-02-20 22:05:59 +0530

    Demo.java Modified

--- Demo.java
+++ Demo.java
@@ -1,6 +1,24 @@
+class A extends Thread{
+    public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println("Thread A: "+i);
+        }
+    }
+}
+class B extends Thread{
+    public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println("Thread B: "+i);
+        }
+    }
+}
+
 
 public class Demo{
     public static void main(String[] args){
-       
+       A a = new A();
+       B b = new B();
+       a.start();
+       b.start();
     }
 }
